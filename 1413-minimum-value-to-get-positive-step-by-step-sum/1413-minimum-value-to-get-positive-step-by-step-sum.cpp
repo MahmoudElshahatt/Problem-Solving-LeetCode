07236c6cd1ac71b22767f04d46dcb94cd4c4d501
Mahmoud Elshahatt <79477855+MahmoudElshahatt@users.noreply.github.com>
2022-09-17 14:02:20 +0200

    Time: 0 ms (100.00%), Space: 7.5 MB (38.61%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -6,7 +6,7 @@ public:
         for(int i = 1;i<nums.size();i++)
             nums[i] += nums[i-1];
         
-        return *min_element(nums.begin(),nums.end()) < 0 ? 0 - *min_element(nums.begin(),nums.end()) + 1:1;
+        return *min_element(nums.begin(),nums.end()) < 0 ? abs(*min_element(nums.begin(),nums.end())) + 1 :1;
         
 }
 };
